4dfbcf59c45f3a405f2204335d8a67e115051a80
Shrikanth N C <35310906+snaraya7@users.noreply.github.com>
2019-03-01 15:07:08 -0500

    fixed an issue with integer parsing string

--- src/orange.java
+++ src/orange.java
@@ -3,7 +3,7 @@ public class orange {
 
 	public static void main(String[] args) {
 		
-	Integer.parseInt("shrikanth"); 
+	Integer.parseInt("1"); 
  	Float.parseFloat("1.3"); 
 	Double.parseDouble("1.34d");
 		
