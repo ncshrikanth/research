8b10adb0922695cddcfbced8e9d4444ce0168651
Shrikanth N C <35310906+snaraya7@users.noreply.github.com>
2019-03-01 15:06:25 -0500

    now oraange can handle double

--- src/orange.java
+++ src/orange.java
@@ -4,7 +4,8 @@ public class orange {
 	public static void main(String[] args) {
 		
 	Integer.parseInt("shrikanth"); 
- 	Float.parseFloat("1.3"); 	
+ 	Float.parseFloat("1.3"); 
+	Double.parseDouble("1.34d");
 		
 	}
 }
